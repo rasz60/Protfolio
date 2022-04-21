444e0efb366ecb68526747f43b01b189bc645635
rasz60 <96821067+rasz60@users.noreply.github.com>
2022-04-21 11:32:56 +0900

    Update portfolio.js

--- resources/js/portfolio.js
+++ resources/js/portfolio.js
@@ -8,73 +8,88 @@ $('.list-group-item a').click(function(e) {
 });
 
 
-$(document).scroll(function(e) {
+$(document).scroll(function() {
 	var boxHeight = $('div#p2').height();
 	var scrollValue = $(document).scrollTop();
+	var s_width = $(window).width();
 	
-	var index = Math.ceil(scrollValue / boxHeight) - 1;
-
-	if ( Number(index) < 0 ) {
-		index = 0;
-	}
-	
-	var limit = Number(scrollValue) % (Number(boxHeight) * Number(index));
-
-	
-	if ( limit > (Number(boxHeight) * 0.4) ) {
-	
-		$('#menu-list ul.list-group li.list-group-item').removeClass('active');	
-		$('#menu-list ul.list-group li.list-group-item').eq(index).addClass('active');
-		
-	} else if ( isNaN(limit) ) {
-		
-		$('#menu-list ul.list-group li.list-group-item').removeClass('active');	
-		$('#menu-list ul.list-group li.list-group-item').eq(0).addClass('active');
-	}
+	scrolling(boxHeight, scrollValue, s_width);
 });
 
 $(document).ready(function() {
 	var width = $(window).width();
+	
+	if( width < 1000 ) {
+		alert('작은 화면에서는 정상적으로 작동하지 않을 수 있습니다.\n많은 양해 부탁드리겠습니다.');
+	}
+
 	collapse(width);
 });
 
 $(window).resize(function() {
 	var e_width = $(window).width();
 	collapse(e_width);
-
-	console.log(e_width);
+	
 })
 
-
-
 function collapse (width) {
-	if ( width < 740 ) {
+	
+	if ( width < 1000 ) {
 		$('#logo img').attr('src', 'resources/images/collapse_logo.png');
-		$('#menu-p9-1 a').text('Page 1');
-		$('#menu-p9-2 a').text('Page 2');
-		$('#menu-p9-3 a').text('Page 3');
-		$('#menu-p9-4 a').text('Page 4');
-		$('#menu-p9-5 a').text('Page 5');
-		$('#menu-p9-6 a').text('Page 6');
-		$('#menu-p9-7 a').text('Page 7');
-		$('#menu-p9-8 a').text('Page 8');
-		$('#menu-p9-9 a').text('Page 9');
+		$('#menu-p9-1 a').text('Pages');
+		$('#menu-p9-2').addClass('d-none');
+		$('#menu-p9-3').addClass('d-none');
+		$('#menu-p9-4').addClass('d-none');
+		$('#menu-p9-5').addClass('d-none');
+		$('#menu-p9-6').addClass('d-none');
+		$('#menu-p9-7').addClass('d-none');
+		$('#menu-p9-8').addClass('d-none');
+		$('#menu-p9-9').addClass('d-none');
 		$('#nav').css('width', '150px');
+		$('.list-group-item').css('padding', '0.4rem 1.25rem');
 		$('div#content').css('margin-left', '150px');
 		
 	} else {
 		
 		$('#logo img').attr('src', 'resources/images/logo.png');
 		$('#menu-p9-1 a').text('Page 1 : Index');
-		$('#menu-p9-2 a').text('Page 2 : Login');
-		$('#menu-p9-3 a').text('Page 3 : Join');
-		$('#menu-p9-4 a').text('Page 4 : Feed (Calendar & Map)');
-		$('#menu-p9-5 a').text('Page 5 : Feed (Post & UserInfo)');
-		$('#menu-p9-6 a').text('Page 6 : Create Plan');
-		$('#menu-p9-7 a').text('Page 7 : Modify Plan');
-		$('#menu-p9-8 a').text('Page 8 : Create Post');
-		$('#menu-p9-9 a').text('Page 9 : Message & Admin');
+		$('#menu-p9-2').removeClass('d-none');
+		$('#menu-p9-3').removeClass('d-none');
+		$('#menu-p9-4').removeClass('d-none');
+		$('#menu-p9-5').removeClass('d-none');
+		$('#menu-p9-6').removeClass('d-none');
+		$('#menu-p9-7').removeClass('d-none');
+		$('#menu-p9-8').removeClass('d-none');
+		$('#menu-p9-9').removeClass('d-none');
 		$('#nav').css('width', '300px');
+		$('.list-group-item').css('padding', '0.75rem 1.25rem');
 		$('div#content').css('margin-left', '300px');
+	}
+}
+
+function scrolling(boxHeight, scrollValue , s_width) {
+
+	var index = Math.ceil(scrollValue / boxHeight) - 1;
+	
+	if ( Number(index) < 0 ) {
+		index = 0;
+	} 
+	
+	if ( s_width < 1000 && Number(index) > 7) {
+		index = 7;
+	}
+
+	var limit = Number(scrollValue) % (Number(boxHeight) * Number(index));
+
+	if ( Number(limit) > Number(boxHeight)*0.4 ) {
+		
+		$('#menu-list ul.list-group li.list-group-item').removeClass('active');	
+		$('#menu-list ul.list-group li.list-group-item').eq(index).addClass('active');
+			
+	} else if ( isNaN(limit) ) {
+			
+		$('#menu-list ul.list-group li.list-group-item').removeClass('active');	
+		$('#menu-list ul.list-group li.list-group-item').eq(0).addClass('active');
+			
 	}
 }
